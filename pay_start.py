ab833df7ab9ddaf50ed65fc41a5f77becb1bac7c
dazamarin2001-jpg <dazamarin2001@gmail.com>
2026-07-21 15:59:11 -0500

    Add focused pay announcement entrypoint

---
+++ pay_start.py
@@ -0,0 +1,5 @@
+"""Railway entrypoint for verified, single-message pay announcements."""
+
+import pay_ping_reliability_patch  # noqa: F401
+import pay_announcement_guard_patch  # noqa: F401
+import main  # noqa: F401,E402
